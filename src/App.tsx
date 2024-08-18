d362f4780caf98810a2a53124fd8363cf2d83d16
Simone Gentili <simone.gentili@codestorm.it>
2024-08-18 09:01:30 +0200

    another draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import html2canvas from "html2canvas";
 import hljs from "highlight.js";
 import "highlight.js/styles/github.css"; // Puoi scegliere altri stili disponibili
 
 const App = () => {
-  const [code, setCode] = useState("");
-  const printRef = React.useRef();
-
+  const [rawCode, setCode] = useState("");
   const handleDownloadImage = async () => {
-    const element = printRef.current;
-    const canvas = await html2canvas(element);
+    const element: HTMLElement | null = document.querySelector(
+      ".language-javascript"
+    );
 
+    const canvas = await html2canvas(element!);
     const data = canvas.toDataURL("image/jpg");
     const link = document.createElement("a");
 
@@ -26,30 +26,41 @@ const App = () => {
     }
   };
 
+  const [highlightedCode, setH] = useState<string>("");
+
   useEffect(() => {
-    hljs.highlightBlock(printRef.current);
-  }, [code]);
+    const stringa = hljs.highlight(rawCode!, {
+      language: "javascript",
+    }).value;
+    setH(stringa);
+  }, [rawCode]);
+
+  function HtmlToReact({ htmlString }: { htmlString: string }) {
+    return <div dangerouslySetInnerHTML={{ __html: htmlString }} />;
+  }
 
   return (
     <div className="sensorario-container light">
       <h1>code2image</h1>
-      <div className="the-code">
-        <textarea
-          onChange={(event) => {
-            setCode(event.target.value);
-          }}
-          value={code}
-          placeholder="Write your code here..."
-          rows={10}
-          cols={50}
-        />
-      </div>
-      <div className="code-output">
-        <pre>
-          <code ref={printRef} className="language-javascript">
-            {code}
-          </code>
-        </pre>
+      <div className="container">
+        <div className="the-code">
+          <textarea
+            onChange={(event) => {
+              setCode(event.target.value);
+            }}
+            value={rawCode}
+            placeholder="Write your code here..."
+            rows={10}
+            cols={50}
+          />
+        </div>
+        <div className="code-output">
+          <pre>
+            <code className="language-javascript">
+              <HtmlToReact htmlString={highlightedCode} />
+            </code>
+          </pre>
+        </div>
       </div>
       <button onClick={handleDownloadImage}>press me</button>
     </div>
